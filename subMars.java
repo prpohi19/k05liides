d9bc5783d9d44cff9f109d1758e59d2523d7373d
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2019-03-20 11:51:56 +0200

    Update subMars.java

--- subMars.java
+++ subMars.java
@@ -1,7 +1,7 @@
 class subMars extends Mars {
 	@Override
 	public void timeToTravelToEarth(){
-		int timeMinutes = (int) (distance / (299792 * 60)); /* km/s km/m */
+	int timeMinutes = (int) (distance / (299792 * 60)); /* km/s km/m */
 	System.out.println("Sõit Marsist Maale tagasi sama kiirusega võtab " + timeMinutes + " minutit.");
 	}	
 }
