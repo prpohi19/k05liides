e93bc68998ea8e1b26f3d1734c0e718ea5021d32
Taavi Liivat <43164710+taavlii@users.noreply.github.com>
2019-05-07 23:27:27 +0300

    printout lisatud

--- Game.java
+++ Game.java
@@ -24,4 +24,39 @@ class Game implements Character {
         }
     }
 }
+/*
+You craft a thing and gain 28 XP!
+You craft a thing and gain 31 XP!
+You craft a thing and gain 10 XP!
+You craft a thing and gain 33 XP!
+You craft a thing and gain 22 XP!
+You craft a thing and gain 31 XP!
+You have leveled up to level 2!! You now need 220 for next level!
+You craft a thing and gain 22 XP!
+You craft a thing and gain 2 XP!
+You craft a thing and gain 4 XP!
+You craft a thing and gain 14 XP!
+You craft a thing and gain 18 XP!
+You craft a thing and gain 15 XP!
+You craft a thing and gain 32 XP!
+You craft a thing and gain 11 XP!
+You craft a thing and gain 12 XP!
+You craft a thing and gain 2 XP!
+You craft a thing and gain 27 XP!
+You craft a thing and gain 34 XP!
+You craft a thing and gain 7 XP!
+You craft a thing and gain 15 XP!
+You craft a thing and gain 27 XP!
+You have leveled up to level 3!! You now need 478 for next level!
+You craft a thing and gain 24 XP!
+You craft a thing and gain 20 XP!
+You craft a thing and gain 24 XP!
+You craft a thing and gain 33 XP!
+You craft a thing and gain 31 XP!
+You craft a thing and gain 2 XP!
+You craft a thing and gain 20 XP!
+You craft a thing and gain 14 XP!
+You craft a thing and gain 16 XP!
+You craft a thing and gain 22 XP!
+*/
 
