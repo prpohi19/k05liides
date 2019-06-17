35154dad1ff6ce870a52e4de5ee13c1606ee6fb6
Revor Jan Nuiamäe <43164694+rnuiame@users.noreply.github.com>
2019-06-17 17:46:47 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args){
+        Dog pontu = new Dog();
+        pontu.sound();
+        pontu.eat();
+        pontu.sleep();
+        Cat miisu = new Cat();
+        miisu.sound();
+        miisu.eat();
+        miisu.sleep();
+    }
+}
+
+/*
+OUTPUT
+koer haugub
+koer sööb konti
+koer magab kuudis
+kass nurrub
+kass sööb hiirt
+kass magab katusel
+*/
