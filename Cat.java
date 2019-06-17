35a0a2475546cbe7560fee83720f0383a0e24024
Revor Jan Nuiamäe <43164694+rnuiame@users.noreply.github.com>
2019-06-17 17:47:37 +0300

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,11 @@
+class Cat implements Animal{
+    public void sound(){
+        System.out.println("kass nurrub");
+    }
+    public void eat(){
+        System.out.println("kass sööb hiirt");
+    }
+    public void sleep(){
+        System.out.println("kass magab katusel");
+    }
+}
